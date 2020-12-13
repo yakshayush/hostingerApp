0f8a41baffcb75f1b49ed1ca5758a007d0615ed5
Sumeet Kumar <sunilkumarmizarsatish@gmail.com>
2020-12-13 11:49:48 +0530

    craeted response UI

--- app.js
+++ app.js
@@ -12,14 +12,17 @@ const app = express();
 //db connections //
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
-    extended:true
+    extended: true
 }));
+const helmet = require("helmet");
+const rateLimiter = require("express-rate-limit");
 
 require("./model/user");
 const dbConfig = config.get('mongoDb.host');
 mongoose.connect(dbConfig, {
     useNewUrlParser: true,
-    useUnifiedTopology: true });
+    useUnifiedTopology: true
+});
 
 mongoose.connection.on("error", () => {
     console.log("> error occurred from the database");
@@ -32,16 +35,18 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set('views', './views');
 app.engine('html', require('ejs').renderFile);
+app.use(helmet);
+app.use(rateLimiter);
 
 const loginRoute = require('./api/routes/login');
 const registerRoute = require('./api/routes/register');
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.render("app");
 });
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 
-app.listen(PORT,() => console.log('server started'));
+app.listen(PORT, () => console.log('server started'));
 
 module.exports = app;
---
+++ utils/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: "942715579365-fpouo8vujnj646ohg5oca5gs6m52t78e.apps.googleusercontent.com",
+        clientSecret: "ghF598pyusmsEjl6i4hBzMI6"
+    },
+    mongodb: {
+        dbURI: "mongodb://127.0.0.1:27017/kite"
+
+
+
+    }
+};
